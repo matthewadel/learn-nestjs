f46465a377faa58576f3c14ed249895ead885078
matthew-adel-vodafone <matthew.awad@vodafone.com>
2025-04-05 15:59:24 +0200

    integrate helmet

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,8 @@ async function bootstrap() {
   app.enableCors({
     origin: 'http://localhost:3000',
   });
+
+  app.use(helmet());
 
   const swagger = new DocumentBuilder()
     .setTitle('Mis APIs')
